15968abd22bcd7622eec2d63c4b0f887521f0c9b
Sadda Hussain <saddabutt@gmail.com>
2024-10-28 19:51:41 +0500

    add jitpack.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.saddahussain:BubbleSeekBar:1.0.0")
+
 }
